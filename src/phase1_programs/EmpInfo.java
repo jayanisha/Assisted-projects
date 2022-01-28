f999f19f6098cdcd5b131ea4e4e980b54d056202
jayanisha <nishajayaraman98@gmail.com>
2022-01-28 12:41:02 +0530

    Programs

---
+++ src/phase1_programs/EmpInfo.java
@@ -0,0 +1,15 @@
+package phase1_programs;
+
+public class EmpInfo {
+		int id;
+		String name;
+
+	void display() {
+		System.out.println(id+" "+name);
+		}
+	}
+
+
+	
+
+	
---
+++ src/phase1_programs/IncSub.java
@@ -0,0 +1,31 @@
+package phase1_programs;
+import java.util.Scanner;
+public class IncSub {
+	public static void main(String[] args) {
+		IncSub s = new IncSub();
+		    s.IncOrder();
+		  }
+		  void IncOrder() {
+		    int num;
+		         boolean flag = false;
+		         Scanner scanner = new Scanner(System.in);
+		         System.out.println("Enter a number : ");
+		         num = scanner.nextInt();
+		         int currentDigit = num % 10;
+		         num = num/10;
+		         while(num>0){
+		             if(currentDigit <= num % 10){
+		                 flag = true;
+		                 break;
+		             }
+
+		             currentDigit = num % 10;
+		             num = num/10;
+		         }
+		         if(flag){
+		             System.out.println("Digits are not in increasing order.");
+		         }else{
+		             System.out.println("Digits are in increasing order.");
+		         }
+		  }
+}
